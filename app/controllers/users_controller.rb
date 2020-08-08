7e15751433ab3ccb0773e4057086618c37fd1531
yuya tanaka <yuya.tanaka0808.w@gmail.com>
2020-08-08 23:51:15 +0900

    ユーザー編集作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,33 @@
 class UsersController < ApplicationController
+before_action :find_user, only:[:show, :edit, :update]
   def new
-      @user = User.new
-    end
-    def create
-      @user = User.new(user_params)
-      if @user.save
-        redirect_to user_path(@user.id)
-      else
-        render :new
-      end
-    end
-    def show
-      @user = User.find(params[:id])
-    end
-
-    private
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :password_digest, :password_confirmation)
+    @user = User.new
+  end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user.id)
+    else
+      render :new
     end
   end
+  def show
+  end
+  def edit
+  end
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user.id),notice: "編集しました！"
+    else
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_digest, :password_confirmation)
+  end
+  def find_user
+    @user = User.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :favorites, only: [:create, :destroy]
   resources :contacts
   resources :sessions, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:new, :create, :update, :edit, :show]
   resources :pictures do
     collection do
       post :confirm
