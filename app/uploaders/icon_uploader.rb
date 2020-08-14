a68fdee1e270fc1d03b8f3a8528a3998502b3cf9
yuya tanaka <yuya.tanaka0808.w@gmail.com>
2020-08-14 10:55:49 +0900

    プロフィール画像機能作成

--- app/uploaders/icon_uploader.rb
+++ app/uploaders/icon_uploader.rb
@@ -30,13 +30,13 @@ class IconUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-   version :thumb do
-     process resize_to_fit: [50, 50, "center"]
-   end
-
-   version :icon do
-     process resize_to_fit: [100, 100, "center"]
-   end
+   # version :thumb do
+   #   process resize_to_fit: [50, 50, "center"]
+   # end
+   #
+   # version :icon do
+   #   process resize_to_fit: [100, 100, "center"]
+   # end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
