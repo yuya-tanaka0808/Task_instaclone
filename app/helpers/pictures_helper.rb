710a6c560d810a65ab0ab1031b853ce59c9c9852
yuya tanaka <yuya.tanaka0808.w@gmail.com>
2020-08-18 06:24:48 +0900

    ヘルパー修正

--- app/helpers/pictures_helper.rb
+++ app/helpers/pictures_helper.rb
@@ -13,4 +13,12 @@ module PicturesHelper
     end
   end
 
+  def confirm_new_or_edit
+    if action_name == 'new' || action_name == 'create'
+      confirm_pictures_path
+    elsif action_name == 'edit' || action_name == 'update'
+      # render confirm_picture_path(@picture.id)
+      picture_path
+    end
+
 end
